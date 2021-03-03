fb40d238d07d6db6a022984b12cd1e4893c0b8e5
Alex Young <younga6@oregonstate.edu>
2021-03-03 09:30:50 -0800

    Wrote second test, this checks if FizzBuzz prints out 'Fizz' if '3' is passed in

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -10,6 +10,8 @@ import FizzBuzz
 class TestFizzBuzz(unittest.TestCase):
     def test1(self):
         self.assertEqual(FizzBuzz.run(1), 1)
+    def test2(self):
+        self.assertEqual(FizzBuzz.run(3), "Fizz")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
