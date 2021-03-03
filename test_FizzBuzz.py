3caf14dc3e30b1837c447b12f0d271096d561b19
Alex Young <younga6@oregonstate.edu>
2021-03-03 09:36:23 -0800

    Wrote third test, this checks if multiples of 5 contain logic to print out Buzz - also slightly updated some previous tests

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -10,8 +10,13 @@ import FizzBuzz
 class TestFizzBuzz(unittest.TestCase):
     def test1(self):
         self.assertEqual(FizzBuzz.run(1), 1)
+        self.assertEqual(FizzBuzz.run(2), 2)
     def test2(self):
         self.assertEqual(FizzBuzz.run(3), "Fizz")
+        self.assertEqual(FizzBuzz.run(6), "Fizz")
+    def test3(self):
+        self.assertEqual(FizzBuzz.run(5), "Buzz")
+        self.assertEqual(FizzBuzz.run(10), "Buzz")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
