0404ae2ef79d71fd43a98965d70da6e0dbb5d313
Alex Young <younga6@oregonstate.edu>
2021-03-03 09:41:37 -0800

    Implemented FizzBuzz method, now run correcly uses mod logic to take into account multiples of 3 and 5 to print out FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -7,7 +7,9 @@
 # printing "Buzz" and multiples of both "FizzBuzz"
 
 def run(x):
-    if (x % 3 == 0):
+    if (x % 3 == 0 and x % 5 == 0):
+        return "FizzBuzz"
+    elif (x % 3 == 0):
         return "Fizz"
     elif (x % 5 == 0):
         return "Buzz"
